3f81a0781c4cf97bf08bf771c764bd2d24941e4d
waleedbutt98 <waleed3072@gmail.com>
2021-02-02 20:37:01 +0500

    Fashion MNIST done

--- Q3/Flask/TheNewBoston/07 Passing Objects into Templates.py
+++ Q3/Flask/TheNewBoston/07 Passing Objects into Templates.py
@@ -4,14 +4,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/<user>')
-def index(user=None):
-    return render_template('user.html', user=user)
+def index():
+    return 'index'
+
 
 @app.route('/shopping')
-def index():
+def shopping():
     food = ['Cheese', 'Fries', 'Coke']
     return render_template('shopping.html', food=food)
+
 
 if __name__ == '__main__':
     app.run()
